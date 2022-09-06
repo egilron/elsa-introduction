1c8c42abfcc99529348ec4dd5ea827ec6f31570d
Egil Rønningstad <egilron@ifi.uio.no>
2022-09-06 18:54:29 +0200

    Begin cleaner

---
+++ clone_repos.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+git clone https://github.com/ltgoslo/norec.git
+# git clone https://github.com/ltgoslo/norec_fine.git
+git clone https://github.com/ltgoslo/norec_sentence.git
+git clone https://github.com/ltgoslo/norec_tsa.git
